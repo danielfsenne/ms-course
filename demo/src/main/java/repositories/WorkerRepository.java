20dbef0776323a2742268d5871aa295eaba2c0c1
danielSenne <danielflausinosenne123@gmail.com>
2026-09-08 21:23:53 -0300

    implementando hr-worker

---
+++ demo/src/main/java/repositories/WorkerRepository.java
@@ -0,0 +1,8 @@
+package repositories;
+
+import entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository <Worker, Long> {
+
+}
