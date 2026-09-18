530b948409453ba1325a44b9dd49b4872efefd5a
danielSenne <danielflausinosenne123@gmail.com>
2026-09-18 08:14:41 -0300

    resilience4j para tolerância e falhas

--- demo/src/main/java/resources/WorkerResource.java
+++ demo/src/main/java/resources/WorkerResource.java
@@ -35,6 +35,15 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        /*
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        */
+
         logger.info("PORT =" + env.getProperty("local.server.port"));
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
