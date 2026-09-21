642528ecb2017ba46b2f94337ee7b4e12b58b709
danielSenne <danielflausinosenne123@gmail.com>
2026-09-21 09:47:39 -0300

    Actuator para atualizar as configurações em runtime

--- demo/src/main/java/resources/WorkerResource.java
+++ demo/src/main/java/resources/WorkerResource.java
@@ -2,6 +2,7 @@ package resources;
 
 import entities.Worker;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,7 @@ import repositories.WorkerRepository;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
