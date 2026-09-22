207aa06f5458e3e695ab7805c30581ba19d7fa2f
danielSenne <danielflausinosenne123@gmail.com>
2026-09-22 09:52:48 -0300

    Userrepository, UserResource e configuração de rotas

---
+++ hr-user/src/main/java/org/hruser/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package org.hruser.repositories;
+
+import org.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
